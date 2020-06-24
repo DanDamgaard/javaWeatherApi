dd247bbb510ed83bfa8e6eb450bb420465f1845c
Dan Thormann Damgaard <DTDa.skp>
2020-06-24 12:58:00 +0200

    made a correction to api data link

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather{
 
   // Fetch weather from api
   async getWeather(){
-    const responce = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&units=metric&appid=b08b3b941a34891f0be81bc4ff7bfa8a`);
+    const responce = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&units=metric&appid=${this.apiKey}`);
 
     const responceData = await responce.json();
 
